9555a899c0154a5a924d4fb79e58950496dc2996
Kona <69055590+Konashii@users.noreply.github.com>
2025-01-10 05:10:56 -0500

    Um.. am I doing this right?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    base: '/'
+    base: '/',
+    vite: {
+        build: {
+            outDir: 'docs'
+        }
+    }
 });
