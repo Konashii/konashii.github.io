4948ab547f1c4be04efd7b13ca727988d1c7fb55
kona <redsonic24@aol.com>
2026-05-13 02:17:10 -0400

    fixing issues with github deployment

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+
+const blogCollection = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/blog/' }),
+
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+}
